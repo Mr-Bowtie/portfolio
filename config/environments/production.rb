e6e6ba6f71b92638186c5ce6848455537635f342
Ian Withers <mr.bowtie989@gmail.com>
2022-02-28 19:45:58 -0500

    fix: trying to fix mailgun error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,17 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #mailgun config
+  ActionMailer::Base.smtp_settings = {
+    :port => ENV['MAILGUN_SMTP_PORT'],
+    :address => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+    :password => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain => 'itnw-portfolio.herokuapp.com', # UPDATE THIS VALUE WITH YOUR OWN APP
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -90,15 +101,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  #mailgun config
-  ActionMailer::Base.smtp_settings = {
-    :port => ENV['MAILGUN_SMTP_PORT'],
-    :address => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
-    :password => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain => 'itnw-portfolio.herokuapp.com', # UPDATE THIS VALUE WITH YOUR OWN APP
-    :authentication => :plain,
-  }
-  ActionMailer::Base.delivery_method = :smtp
 end
