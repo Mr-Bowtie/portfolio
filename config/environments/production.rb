e3a595bb0499422d6481de9ccf39e7bb0670a65a
Ian Withers <mr.bowtie989@gmail.com>
2022-02-28 21:00:06 -0500

    fix: testing fix for sending mail errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,15 +4,15 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   #mailgun config
-  ActionMailer::Base.smtp_settings = {
-    :port => ENV['SENDGRID_SMTP_PORT'],
-    :address => ENV['SENDGRID_SERVER'],
-    :user_name => ENV['SENDGRID_LOGIN'],
-    :password => ENV['SENDGRID_PASSWORD'],
+  config.action_mailer.smtp_settings = {
+    :port => ENV['MAILGUN_SMTP_PORT'],
+    :address => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+    :password => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain => 'ianwithers.dev', # UPDATE THIS VALUE WITH YOUR OWN APP
     :authentication => :plain,
   }
-  ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
 
   # Code is not reloaded between requests.
   config.cache_classes = true
