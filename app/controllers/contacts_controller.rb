c39778ee428f5f91e3688da4977d7547c5e63560
Ian Withers <mr.bowtie989@gmail.com>
2023-06-02 10:27:07 -0400

    chore: remove comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,12 +7,9 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      # flash.now[:notice] = 'Message sent!'
       redirect_to portfolio_display_path, notice: 'Message sent!'
     else
-      # flash.now[:alert] = 'Could not send message'
       render :new, status: :unprocessable_entity
-      # render 'portfolio/display', status: :unprocessable_entity
     end
   end
 end
