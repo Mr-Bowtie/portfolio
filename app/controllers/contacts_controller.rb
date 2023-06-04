0ee854011d9c009bbc63539332b8b314997faad8
Ian Withers <mr.bowtie989@gmail.com>
2023-06-04 14:59:32 -0400

    fix: add better error handling for mail server errors

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,9 +6,18 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
-    if @contact.deliver
-      redirect_to portfolio_display_path, notice: 'Message sent!'
-    else
+    begin
+      if @contact.deliver
+        redirect_to portfolio_display_path, notice: 'Message sent!'
+      else
+        flash.alert = 'Invalid form'
+        render :new, status: :unprocessable_entity
+      end
+    rescue Net::SMTPFatalError
+      flash.alert = 'Mail Server Error: try again later'
+      render :new, status: :unprocessable_entity
+    rescue => e
+      flash.alert = "Unexpected Error : #{e} "
       render :new, status: :unprocessable_entity
     end
   end
