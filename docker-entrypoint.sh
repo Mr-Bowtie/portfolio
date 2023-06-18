f8c6568fabf289c35607ede2d01fed041dbc7f82
Ian Withers <mr.bowtie989@gmail.com>
2023-06-18 16:42:20 -0400

    fix: check for server lock file existance before trying to delete to avoid error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,13 @@ set -e
 # sleep infinity
 
 # server isnt shutting down nicely and managing this file lock, so Im manually doing it for now
-rm ./tmp/pids/server.pid
+file_path="./tmp/pids/server.pid"  # Replace with the actual path to your file
+
+if [ -e "$file_path" ]; then
+    rm "$file_path"
+    echo "File deleted: $file_path"
+else
+    echo "No server lock to worry about"
+fi
 bin/rails db:prepare
 bin/rails s -b 0.0.0.0
