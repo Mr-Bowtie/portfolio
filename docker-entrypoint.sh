e8ef02828d814269cee2573f73729d2c5a4b1178
Ian Withers <mr.bowtie989@gmail.com>
2023-06-17 22:54:07 -0400

    update: docker compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,7 @@ set -e
 # use to start container with out executing any commands immediately, useful for debugging
 # sleep infinity
 
+# server isnt shutting down nicely and managing this file lock, so Im manually doing it for now
+rm ./tmp/pids/server.pid
 bin/rails db:prepare
 bin/rails s -b 0.0.0.0
